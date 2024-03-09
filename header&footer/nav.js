3517cd30ee800a286a4c82c805382f0095133550
Raushan <raushankumar23082004@gmail.com>
2024-03-09 21:35:11 +0530

    not working for all page login signin page

---
+++ header&footer/nav.js
@@ -0,0 +1,12 @@
+
+    document.addEventListener('DOMContentLoaded', function () 
+    {
+        var nav = document.querySelector(".nav-class-for-phone");
+        var checkbox = document.getElementById('check');
+            document.addEventListener('click', function (event) {
+            if (!nav.contains(event.target) && checkbox.checked)
+            {
+                checkbox.checked = false;
+            }
+        });
+    });
