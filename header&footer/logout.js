0f6aa220757cc8baab685fb4900feac5feef4892
Raushan <raushankumar23082004@gmail.com>
2024-06-08 10:57:19 +0530

    udpate

--- header&footer/logout.js
+++ header&footer/logout.js
@@ -2,6 +2,6 @@ const signoutBtn = document.querySelector("#signout");
 let userInfo = JSON.parse(localStorage.getItem("user"));
 let Signout = () => {
     localStorage.removeItem("user");
-    window.location.href = 'index.html';
+    window.location.href = '/index.html';
 }
 signoutBtn.addEventListener('click', Signout);
